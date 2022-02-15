f766f36cee274923910617bdc42e565944a68105
Diana Maslyuk <di.maslyuk@gmail.com>
2022-02-15 21:48:42 +0100

    moi first commit

---
+++ add.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 25.01.2022
+ * @author 
+ */
+
+public class add {
+  
+  public static void main(String[] args) {
+    
+    int[]arr = new int[5];
+    arr[0]=1;
+    arr[1]=1 2;
+    arr[2]=1 2 3;
+    arr[3]=1 2 3 4;
+    arr[4]=1 2 3 4 5;
+    
+    System.out.println(arr[5]);
+  } // end of main
+
+} // end of class add
+
